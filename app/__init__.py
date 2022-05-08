bf79e7f27bc63d7fef653ec3c9669f4e5dd713df
gladyswambura <gladyswahito7@gmail.com>
2022-05-08 09:14:23 +0300

    registers blueprints of main and auth

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap    
 from flask_mail import Mail
+from config import config_options
 
 app = Flask(__name__)     
 
@@ -14,5 +15,18 @@ bootstrap = Bootstrap(app)
 mail = Mail(app)
 login_manager.login_view = 'auth.login'
 login_manager.session_protection = 'strong'
+
+def create_app(config_name):
+    app.config.from_object(config_options[config_name])
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
 
 
