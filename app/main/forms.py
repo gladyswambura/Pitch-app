5f1d16001edbc0772fae5b071381bb6161b44631
gladyswambura <gladyswahito7@gmail.com>
2022-05-10 08:10:16 +0300

    creates a flask form with categories

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import InputRequired
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired()])
     post = TextAreaField('Pitch', validators=[InputRequired()])
-    category = SelectField('Category', choices=[('PRODUCT', 'PRODUCT'), ('IDEA', 'IDEA'), ('Business', 'Business')],
+    category = SelectField('Category', choices=[('Product', 'Product'), ('Idea', 'Idea'), ('Business', 'Business'), ('School', 'School')],
                            validators=[InputRequired()])
     submit = SubmitField('Post')
 
